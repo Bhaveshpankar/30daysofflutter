fce3119f910297607d75deb49c72e173eb86cf66
Bhaveshpankar <bhavpankar@gmail.com>
2022-04-13 23:59:33 +0530

    day _10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,24 @@
+class item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  item({required this.id, required this.name, required this.desc, required this.price, required this.color, required this.image) {
+    // TODO: implement 
+    throw UnimplementedError();
+  }
+
+  final product = [
+    item(
+      id : "codepur001",
+      name: "iphone 12 Pro",
+      desc: "apple iphone 12th generator",
+      price: 999,
+      color: "#35505a",
+      image: "https://m.media-amazon.com/images/I/71umuN8XVeL.jpg")
+
+  ];
+}
