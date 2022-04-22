3a24843bd10df4743cc053315aa2df91e0d66905
Bhaveshpankar <bhavpankar@gmail.com>
2022-04-22 16:42:07 +0530

    day_14 on hold

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -22,32 +22,40 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    loadData() async {
-      final catalogJson =
-          await rootBundle.loadString("assets/files/catalog.json");
-      final decodedData = jsonDecode(catalogJson);
-      var productsData = decodedData["products"];
-      print(productsData);
-    }
+    loadData();
+  }
+
+  loadData() async {
+    // await Future.delayed(Duration(seconds: 2));
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Catalog App"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          },
-        ),
+        child: (CatalogModel.items!=null && CatalogModel.items!.isNotEmpty)?  ListView.builder(
+          itemCount: CatalogModel.items?.length,
+          itemBuilder: (context, index) =>
+            ItemWidget(
+              item: CatalogModel.items![index],
+            ),
+          
+        )
+        :Center(
+          child: CircularProgressIndicator(),
+       ),
       ),
       drawer: const Mydrawer(),
     );
--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,17 +1,11 @@
-// ignore_for_file: camel_case_types
+// ignore_for_file: unnecessary_null_comparison
 
-import 'package:flutter/foundation.dart';
+import 'dart:convert';
+
+
 
 class CatalogModel {
-  static final items = [
-    Item(
-        id: 1,
-        name: "iphone 12 Pro",
-        desc: "apple iphone 12th generator",
-        price: 999,
-        color: "#35505a",
-        image: "https://m.media-amazon.com/images/I/71umuN8XVeL.jpg")
-  ];
+  static List<Item>? items;
 }
 
 class Item {
@@ -22,31 +16,87 @@ class Item {
   final String color;
   final String image;
 
-  Item(
-      {required this.id,
-      required this.name,
-      required this.desc,
-      required this.price,
-      required this.color,
-      required this.image});
+  Item({
+    required this.id,
+    required this.name,
+    required this.desc,
+    required this.price,
+    required this.color,
+    required this.image,
+  });
 
-  factory Item.fromMap(Map<String, dynamic> map) {
+  Item copyWith({
+    int? id,
+    String? name,
+    String? desc,
+    num? price,
+    String? color,
+    String? image,
+  }) {
     return Item(
-      id: map["id"],
-      name: map["name"],
-      desc: map["desc"],
-      price: map["price"],
-      color: map["color"],
-      image: map["image"],
+      id: id ?? this.id,
+      name: name ?? this.name,
+      desc: desc ?? this.desc,
+      price: price ?? this.price,
+      color: color ?? this.color,
+      image: image ?? this.image,
     );
   }
 
-  toMap() => {
-        "id": id,
-        "name": name,
-        "desc": desc,
-        "price": price,
-        "color": color,
-        "image": image
-      };
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'desc': desc,
+      'price': price,
+      'color': color,
+      'image': image,
+    };
+  }
+
+  factory Item.fromMap(Map<String, dynamic> map) {
+    
+
+    return Item(
+      id:map['id'],
+      name:map['name'],
+      desc: map['desc'],
+      price: map['price'],
+      color: map['color'],
+      image: map['image'],
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Item.fromJson(String source) =>
+      Item.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Item(id: $id, name: $name, desc: $desc, price: $price, color: $color, image: $image)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Item &&
+        other.id == id &&
+        other.name == name &&
+        other.desc == desc &&
+        other.price == price &&
+        other.color == color &&
+        other.image == image;
+  }
+
+  @override
+  int get hashCode {
+    return id.hashCode ^
+        name.hashCode ^
+        desc.hashCode ^
+        price.hashCode ^
+        color.hashCode ^
+        image.hashCode;
+  }
 }
